2a9a6ba74f52d1b25efe2ccd326e0e2a6172785d
B <77552838+Mrballer59@users.noreply.github.com>
2022-04-20 21:12:55 +0100

    linked the path + started CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import React from "react";
-
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonial/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Services from "./components/services/Services";
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
+export default App;
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import "./about.css";
 
 export const about = () => {
   return <div>about</div>;
 };
+export default about;
