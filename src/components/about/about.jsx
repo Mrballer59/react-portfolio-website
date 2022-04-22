21a856f1c071127065e599ba35548d6a1b2c68a7
B <77552838+Mrballer59@users.noreply.github.com>
2022-04-22 15:09:17 +0100

    MQ + about added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 import "./about.css";
+import Me from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
-export const about = () => {
-  return <section id="about">about</section>;
+export const About = () => {
+  return (
+    <section id="about">
+      <h5>Get to Know </h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ Years working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Client</h5>
+              <small>50+ WorldWide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ completed Projects </small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            inventore magni at iusto quasi maiores. Incidunt, est. Illum facilis
+            aliquid amet velit voluptate eum placeat.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
-export default about;
+export default About;
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,7 +1,79 @@
 import React from "react";
 import "./experience.css";
-const experience = () => {
-  return <section id="experience">experience</section>;
+import { HiBadgeCheck } from "react-icons/hi";
+const Experience = () => {
+  return (
+    <section id="experience">
+      <h5>The Skills I Have </h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>FrontEnd Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>SaSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>BackEnd Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Trash</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default experience;
+export default Experience;
