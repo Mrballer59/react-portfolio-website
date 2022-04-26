b23b73ab3bba7bd713ca9b09f0290a4cb20c92fa
B <77552838+Mrballer59@users.noreply.github.com>
2022-04-26 15:45:48 +0100

    Footer added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,32 +2,74 @@ import React from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsLinkedin } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
-const contact = () => {
+const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_l5n4htw",
+        "template_vixshjv",
+        form.current,
+        "_PE7vp_OaKZHoXRsr"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
-        <div className="contact__option">
+        <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
 
-            <a href="mailto: Brandon.JR.Dev@hotmail.com">Send a message</a>
+            <a href="mailto: Brandon.JR.Dev@gmail.com">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsLinkedin />
+            <BsLinkedin className="contact__option-icon" />
             <h4>Linked In </h4>
             <h5>just in case you didn't see it above</h5>
-            <a href="https://linkedin.com/">Check it out</a>
+            <a href="https://linkedin.com/" target={"_blank"}>
+              Check it out
+            </a>
           </article>
         </div>
         {/* End of contact options */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            rows="10"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send message
+          </button>
+        </form>
       </div>
     </section>
   );
 };
 
-export default contact;
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 
-const footer = () => {
-  return <div>footer</div>;
+const Footer = () => {
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        My logo
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">about</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https//facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https//instagram.com">
+          <FiInstagram />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Brandon Joseph R. All rights reserved</small>
+      </div>
+    </footer>
+  );
 };
 
-export default footer;
+export default Footer;
